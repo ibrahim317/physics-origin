bf3ad479c102e3539e5d7dd333e8441b0d1d41f2
ibrahim <zeroibrahim49@gmail.com>
2023-11-21 13:10:01 +0200

    Add typescript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,11 @@ export const metadata = {
   description: 'منصة احمد يوسف لتدريس منهج الفيزياء للصفوف الثانوية',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import "@/app/global.css"
+import "./global.css"
 
 export default function Home() {
   return (
