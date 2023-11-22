0b263cf20aead8079ae0d16a1745dd7c5ad5bc9a
ibrahim <zeroibrahim49@gmail.com>
2023-11-22 09:42:58 +0200

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Alexandria } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Alexandria } from "next/font/google";
+import "./globals.css";
 
-const inter = Alexandria({ weight: '500', subsets: ['latin'] })
+const inter = Alexandria({ weight: "500", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Physics Origin',
-  description: 'I love potatoes',
-}
+  title: "Physics Origin",
+  description: "منصة احمد يوسف لتدريش منهج الفيزياء للصفوف الثانوية",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
