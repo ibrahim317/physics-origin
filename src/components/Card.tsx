429ca9d5a2effebc8b2ebc5a3bc4e084d7b7a3e5
ibrahim <zeroibrahim49@gmail.com>
2023-11-25 23:34:17 +0200

    Added courceSection

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import CourceImage from "@/public/assets/cource.jpg";
+import Image from "next/image";
+const Card = () => {
+  return (
+    <div
+      className={`min-h-[500px] flex flex-col p-3 gap-5 rounded-md`}
+      style={{ backgroundColor: `#111827` }}
+    >
+      <Image src={CourceImage} alt="courceImage" className="w-full" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi eum
+        magnam molestias qui pariatur doloribus a libero distinctio molestiae
+        illo non tempore corporis inventore, ullam reiciendis tempora
+        necessitatibus! Incidunt, sit!
+      </p>
+    </div>
+  );
+};
+
+export default Card;
