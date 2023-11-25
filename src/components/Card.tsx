0d2a78706845fa15b3289f1eeae1f4fd285ccbbf
ibrahim <zeroibrahim49@gmail.com>
2023-11-26 00:09:33 +0200

    Update Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Card = () => {
   return (
     <div
-      className={`min-h-[500px] flex flex-col p-3 gap-5 rounded-md`}
+      className={`min-h-[500px] hover:-translate-x-4 hover:-translate-y-4 duration-500 ease-in-out flex flex-col p-3 gap-5 rounded-md`}
       style={{ backgroundColor: `#111827` }}
     >
       <Image src={CourceImage} alt="courceImage" className="w-full" />
