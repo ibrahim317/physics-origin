841f505bc136f22a1791c2bdf4b4e94d963ffdd0
M1ghtyMushroom <hilariousbananas@proton.me>
2023-11-25 03:33:06 +0200

    Login Authentication Compleleted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={classnames}>
-        <SessionProvider session={session}> {/*/ This is what keeps the session /*/}
+        <SessionProvider session={session}> {/*///// This is what keeps the session /////*/}
           <Toaster position="top-right" reverseOrder />
           {children}
         </SessionProvider>
