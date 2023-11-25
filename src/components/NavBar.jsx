ec13409e52702bb1abb6968690cf15da326cf1b4
M1ghtyMushroom <hilariousbananas@proton.me>
2023-11-25 04:08:31 +0200

    added a navbar for a logged in user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,24 @@ const NavBar = async () => {
   const session = await getServerSession()
 
   if (session) {
-    return null;
+    return (
+      <nav className="fixed flex items-center justify-around w-full h-28 px-20">
+      <div className="container flex w-fit justify-around mx-10 drop-shadow-md shadow-lg rounded-[50px] bg-black bg-opacity-20 py-4">
+        <Link
+          href="/"
+          className="px-10 max-sm:px-5 py-4 mx-4 min-w-max hover:text-[#F9C500] transition ease-in-out duration-300"
+        >
+          الكورسات
+        </Link>
+        <Link
+          href="/profile"
+          className="px-10 max-sm:px-5 py-4 mx-4 min-w-max hover:text-[#F9C500] transition ease-in-out duration-300"
+        >
+          الحساب الشخصي
+        </Link>
+      </div>
+    </nav>
+    );
   }
 
   return (
