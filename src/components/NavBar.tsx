e1f93e1e02908388083b613ea77b584f3e15feb2
ibrahim <zeroibrahim49@gmail.com>
2023-11-26 18:05:51 +0200

    Add logo to Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import React from "react";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import logo from "@/public/assets/logo.png";
 
 const NavBar = () => {
   const { data: session } = useSession();
@@ -35,19 +37,31 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="absolute flex items-center w-full h-24  justify-center drop-shadow-md shadow-lg bg-black bg-opacity-20 py-4">
-        <Link
-          href="/login"
-          className="px-10 max-sm:px-5 py-4 mx-4 min-w-max hover:text-[#F9C500] transition ease-in-out duration-300"
-        >
-          سجل الدخول
+      <nav className="absolute flex justify-around max-sm:justify-between items-center w-full h-24  drop-shadow-md shadow-lg bg-black bg-opacity-20 py-4">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="origin logo"
+            className="max-sm:mx-6"
+            width={50}
+            height={50}
+          />
         </Link>
-        <Link
-          href="/register"
-          className="px-10 max-sm:px-5 py-4 mx-4 min-w-max text-black bg-[#F9C500] rounded-[25px] hover:bg-[#ffffff] transition ease-in-out duration-300"
-        >
-          !اشترك الان
-        </Link>
+        <div className="flex flex-row">
+          <Link
+            href="/login"
+            className="px-10 max-sm:px-5 max-sm:hidden py-4 mx-4 min-w-max hover:text-[#F9C500] transition ease-in-out duration-300"
+          >
+            سجل الدخول
+          </Link>
+          <Link
+            href="/register"
+            className="px-10 max-sm:px-5 max-sm:hidden py-4 mx-4 min-w-max text-black bg-[#F9C500] rounded-[25px] hover:bg-[#ffffff] transition ease-in-out duration-300"
+          >
+            !اشترك الان
+          </Link>
+          <p className="sm:hidden mx-6">menu</p>
+        </div>
       </nav>
       <div className="h-24"></div>
     </>
