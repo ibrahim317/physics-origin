dfe55c8d79d0f9e7314afc65568fab362c43c45e
ibrahim <zeroibrahim49@gmail.com>
2023-11-27 23:35:32 +0200

    Add protocol to api

--- src/lib/getCourse.ts
+++ src/lib/getCourse.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const getCourse = async (url: string) => {
   const URL = "/api" + url;
   const instance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    baseURL: "http://" + process.env.NEXT_PUBLIC_VERCEL_URL,
   });
   try {
     const courses = await instance.get(URL);
