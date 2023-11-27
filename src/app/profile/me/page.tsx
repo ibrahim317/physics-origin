42d104db2657073771715b09a41323d0f28e705d
M1ghtyMushroom <hilariousbananas@proton.me>
2023-11-27 03:40:37 +0200

    responsive

--- src/app/profile/me/page.tsx
+++ src/app/profile/me/page.tsx
@@ -10,8 +10,8 @@ const profile = () => {
 // [#F9C500]
   return (
     <>
-       <div className="container h-96 w-full flex"> {/*/ the container /*/}
-        <div className=" w-1/2 h-full flex justify-center items-center border-r-2"> {/*/ left side /*/}
+       <div className="h-96 w-full flex"> {/*/ the container /*/}
+        <div className=" w-1/2 h-full hidden md:flex justify-center items-center"> {/*/ left side /*/}
           <Image
             className=" rounded-[200px]"
             src={"/assets/person.png"}
@@ -21,7 +21,7 @@ const profile = () => {
           />
         </div>
 
-        <div className="w-1/2 h-full flex flex-col justify-center items-center"> {/*/ right side /*/}
+        <div className="w-full md:w-1/2 h-full flex flex-col justify-center items-center"> {/*/ right side /*/}
           <h1 className="text-5xl text-right flex">!مرحبا يا بطل<span className="text-[#F9C500] text-3xl mx-6"><CheckIcon width={60} height={60}/></span></h1>
           <h2 className="text-3xl flex my-10">{session?.user?.email}<span className="text-[#F9C500] text-3xl mx-6"><EnvelopeClosedIcon width={40} height={40}/></span></h2>
           <button onClick={() => signOut()} className="text-xl flex hover:text-[#f09f94] px-10 py-5 transition ease-in-out duration-300">تسجيل خروج<span><ArrowLeftIcon width={40} height={40} className="mx-5"/></span></button>
