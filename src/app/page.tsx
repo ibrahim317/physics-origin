ffa0d8b9ad04fb1c96e4b1660a4ef8e376f8fb68
ibrahim <zeroibrahim49@gmail.com>
2023-11-30 11:42:43 +0200

    Add paymob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import HeroSection from "../components/HeroSection";
 import CoursesSection from "../components/CoursesSection";
 import Devider from "../components/Devider";
+import asd from "../lib/test";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const re = await asd();
+  redirect(re);
   return (
     <>
       <HeroSection />
