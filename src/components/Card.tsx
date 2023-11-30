8caae65e270149326b76120b6c5c076a1dd98a2b
M1ghtyMushroom <hilariousbananas@proton.me>
2023-11-30 03:12:17 +0200

    Custimized Cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Devider from "./Devider";
 
 const Card = (props: any) => {
   if (props.type == "course") {
@@ -15,7 +16,7 @@ const Card = (props: any) => {
           src={props.content.thumbnail}
           width={300}
           height={300}
-          alt="courceImage"
+          alt="CourseImage"
           className="w-full max-h-[70%] rounded-md"
         />
         <h2 className="text-3xl opacity-80 font-bold">{props.content.name}</h2>
@@ -35,10 +36,13 @@ const Card = (props: any) => {
         className={`min-h-[500px] max-sm:min-h-[350px] hover:border-solid hover:border-y-2 hover:border-white  hover:-translate-y-4 transition-transform duration-500 ease-in-out flex flex-col p-3 gap-5 rounded-lg`}
         style={{ backgroundColor: `#111827` }}
       >
-        <h2 className="text-3xl opacity-80 font-bold">{props.content.name}</h2>
-        <p className="font-light opacity-70 text-lg ">
-          {props.content.des}
-        </p>
+        <div className="flex flex-col justify-center">
+          <h2 className="text-4xl opacity-80 font-bold p-10">{props.content.name}</h2>
+          <div className="-my-5"><Devider /></div>
+          <p className="font-light opacity-70 text-lg p-10">
+            {props.content.des}
+          </p>
+        </div>
       </Link>
     );
   }
