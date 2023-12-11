dd0d5f04b49c842f6d25379bc79d6ff440aa8459
ibrahim <zeroibrahim49@gmail.com>
2023-12-11 21:46:30 +0200

    Add data types

---
+++ src/types/global.d.ts
@@ -0,0 +1,45 @@
+import { $Enums } from "@/prisma/generated/client";
+
+export interface UserType {
+  isAdmin: boolean;
+  id: number;
+  first_name: string;
+  last_name: string;
+  phone: string;
+  parent_phone: string;
+  email: string;
+  pass: string;
+  courses: number[];
+  lectures: number[];
+  buying?: string | null;
+}
+
+export interface CourseType {
+  id: number;
+  name: string;
+  des: string[];
+  price: number;
+  thumbnail: string;
+  section: Section[];
+}
+
+export interface SectionType {
+  id: number;
+  name: string;
+  des: string[];
+  tag: Tag;
+  thumbnail?: string | null;
+  yourlink?: string | null;
+  course: Course[];
+  questions: Question[];
+}
+
+export interface QuestionType {
+  id: number;
+  text: string;
+  options: string[];
+  image?: string | null;
+  quiz: Section[];
+}
+
+const Tag = $Enums.Tag;
