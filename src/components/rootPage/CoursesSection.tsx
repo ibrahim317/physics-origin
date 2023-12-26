9f98602a8154c1b202a85a5b15350e4862744069
ibrahim <zeroibrahim49@gmail.com>
2023-12-26 19:58:51 +0200

    Made cources and lecture sections more responsive

--- src/components/rootPage/CoursesSection.tsx
+++ src/components/rootPage/CoursesSection.tsx
@@ -3,30 +3,30 @@ import Card from "../Card";
 import get_all_courses from "@/src/lib/get_all_courses";
 import { CourseType } from "@/src/types/global";
 const CoursesSection = async () => {
-  const courses = await get_all_courses();
-  return (
-    <section
-      className=" bg-[#1c232d] p-3 flex flex-col rtl "
-      id="coursesSection"
-    >
-      {courses.length > 0 ? (
-        <>
-          <div className="flex bg-[#1c232d] max-lg:justify-center ">
-            <h2 className="text-7xl text-white p-10  ">الكورسات</h2>
-          </div>
-          <div className=" grid gap-6  p-6 grid-cols-3 max-[500px]:grid-cols-1 max-lg:grid-cols-2">
-            {courses.map((course: CourseType) => (
-              <Card type="course" content={course} key={course.id} />
-            ))}
-          </div>
-        </>
-      ) : (
-        <h2 className="font-extrabold my-6 max-sm:text-center text-3xl  max-[420px]:text-4xl">
-          لا توجد كورسات حالياً....ترقب نزولها قريباً
-        </h2>
-      )}
-    </section>
-  );
+	const courses = await get_all_courses();
+	return (
+		<section
+			className=" bg-[#1c232d] p-3 flex flex-col rtl "
+			id="coursesSection"
+		>
+			{courses.length > 0 ? (
+				<>
+					<div className="flex bg-[#1c232d] max-lg:justify-center ">
+						<h2 className="text-7xl text-white p-10  ">الكورسات</h2>
+					</div>
+					<div className="min-h-[50%] grid gap-6  p-6 grid-cols-3 max-md:grid-cols-1 max-lg:grid-cols-2">
+						{courses.map((course: CourseType) => (
+							<Card type="course" content={course} key={course.id} />
+						))}
+					</div>
+				</>
+			) : (
+				<h2 className="font-extrabold my-6 max-sm:text-center text-3xl  max-[420px]:text-4xl">
+					لا توجد كورسات حالياً....ترقب نزولها قريباً
+				</h2>
+			)}
+		</section>
+	);
 };
 
 export default CoursesSection;
--- src/components/rootPage/LecturesSection.tsx
+++ src/components/rootPage/LecturesSection.tsx
@@ -5,47 +5,47 @@ import { get_user_by_email } from "../../lib/get_user";
 import { getServerSession } from "next-auth";
 
 const LecturesSection = async () => {
-  const lectures = await get_all_lectures();
-  let paid_lectures: number[] = [];
+	const lectures = await get_all_lectures();
+	let paid_lectures: number[] = [];
 
-  if (lectures.length > 0) {
-    const session = await getServerSession();
-    if (session) {
-      const user = await get_user_by_email(session?.user?.email);
-      paid_lectures = user?.lectures ?? [];
-    }
-    return (
-      <section
-        className=" bg-[#1c232d] p-3 flex flex-col rtl "
-        id="lecturesSection"
-      >
-        <div className="flex bg-[#1c232d] max-lg:justify-center ">
-          <h2 className="text-7xl text-white p-10  ">المحاضرات</h2>
-        </div>
-        <div className="min-h-[50%] grid gap-6  p-6 grid-cols-3 max-[500px]:grid-cols-1 max-lg:grid-cols-2">
-          {lectures.map((lecture: any) => {
-            const paid = paid_lectures.includes(lecture.id);
-            return (
-              <Card
-                type="lecture"
-                content={lecture}
-                paid={paid}
-                key={lecture.id}
-              />
-            );
-          })}
-        </div>
-      </section>
-    );
-  } else {
-    return (
-      <section className="bg-[#1c232d] p-3 rtl flex flex-col" id="Content">
-        <h2 className="font-extrabold my-6 max-sm:text-center text-3xl  max-[420px]:text-4xl">
-          لا توجد محاضرات حالياً....ترقب نزولها قريباً
-        </h2>
-      </section>
-    );
-  }
+	if (lectures.length > 0) {
+		const session = await getServerSession();
+		if (session) {
+			const user = await get_user_by_email(session?.user?.email);
+			paid_lectures = user?.lectures ?? [];
+		}
+		return (
+			<section
+				className=" bg-[#1c232d] p-3 flex flex-col rtl "
+				id="lecturesSection"
+			>
+				<div className="flex bg-[#1c232d] max-lg:justify-center ">
+					<h2 className="text-7xl text-white p-10  ">المحاضرات</h2>
+				</div>
+				<div className="min-h-[50%] grid gap-6  p-6 grid-cols-3 max-md:grid-cols-1 max-lg:grid-cols-2">
+					{lectures.map((lecture: any) => {
+						const paid = paid_lectures.includes(lecture.id);
+						return (
+							<Card
+								type="lecture"
+								content={lecture}
+								paid={paid}
+								key={lecture.id}
+							/>
+						);
+					})}
+				</div>
+			</section>
+		);
+	} else {
+		return (
+			<section className="bg-[#1c232d] p-3 rtl flex flex-col" id="Content">
+				<h2 className="font-extrabold my-6 max-sm:text-center text-3xl  max-[420px]:text-4xl">
+					لا توجد محاضرات حالياً....ترقب نزولها قريباً
+				</h2>
+			</section>
+		);
+	}
 };
 
 export default LecturesSection;
