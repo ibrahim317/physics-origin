7868031afde4534d060cf49780c7de2c60d4cd24
ibrahim <zeroibrahim49@gmail.com>
2023-12-26 19:59:52 +0200

    Update the db

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,52 +1,53 @@
 import { $Enums } from "@/prisma/generated/client";
+import { JsonValue } from "next-auth/adapters";
 
 export interface UserType {
-  isAdmin: boolean;
-  id: number;
-  first_name: string;
-  last_name: string;
-  phone: string;
-  parent_phone: string;
-  email: string;
-  pass: string;
-  courses: number[];
-  lectures: number[];
-  buying?: string | null;
-  progress?: Progress | null;
+	isAdmin: boolean;
+	id: number;
+	first_name: string;
+	last_name: string;
+	phone: string;
+	parent_phone: string;
+	email: string;
+	pass: string;
+	courses: number[];
+	lectures: number[];
+	buying?: string | null;
+	progress?: Progress | null;
 }
 export interface Progress {
-  userId: number;
-  passed_tests: number[];
-  tests_grades: number[];
+	userId: number;
+	passed_quizzes: number[];
+	quizzes_grades: JsonValue;
 }
 
 export interface CourseType {
-  id: number;
-  name: string;
-  des: string[];
-  price: number;
-  thumbnail: string;
-  section: Section[];
+	id: number;
+	name: string;
+	des: string[];
+	price: number;
+	thumbnail: string;
+	section: Section[];
 }
 
 export interface SectionType {
-  id: number;
-  name: string;
-  des: string[];
-  tag: Tag;
-  thumbnail?: string | null;
-  yourlink?: string | null;
-  course: Course[];
-  questions: Question[];
-  price?: number | null;
+	id: number;
+	name: string;
+	des: string[];
+	tag: Tag;
+	thumbnail?: string | null;
+	yourlink?: string | null;
+	course: Course[];
+	questions: Question[];
+	price?: number | null;
 }
 
 export interface QuestionType {
-  id: number;
-  text: string;
-  options: string[];
-  image?: string | null;
-  quiz: Section[];
+	id: number;
+	question_head: string;
+	options: string[];
+	image?: string | null;
+	quiz: Section[];
 }
 
 const Tag = $Enums.Tag;
